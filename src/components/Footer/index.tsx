247a7ec869f9d2d3aa9831c117baf8982c1e1c4e
PedroMakengo <pedromakengomiguel@gmail.com>
2024-05-22 17:04:19 +0100

    Adicionado a estrutura base do meu projeto Mora-Commuty

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+import { styles } from './styles'
+
+export function Footer() {
+  return (
+    <View>
+      <Text>Footer</Text>
+    </View>
+  )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+import { styles } from './styles'
+
+export function Header() {
+  return (
+    <View>
+      <Text>Footer</Text>
+    </View>
+  )
+}
